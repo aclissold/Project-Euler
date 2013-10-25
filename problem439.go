2bc30bd9ebfbad2311c2b5b3460892f0dd622c9d
Andrew Clissold <andrewclissold@gmail.com>
2013-10-25 09:44:49 -0400

    Add comments to problem439

--- problem439.go
+++ problem439.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-// divisorSums holds the previously-computed values of d(k)
+// divisorSums holds the previously-computed values of d(k) (a map is basically a Python dictionary)
 var divisorSums = make(map[int]int)
 
 func main() {
@@ -21,15 +21,16 @@ func S(N int) int {
 	divisorSum := 0
 	for i := 1; i <= N; i++ {
 		for j := 1; j <= N; j++ {
-			startTime := time.Now()
-			if computedSum, ok := divisorSums[i*j]; ok {
+			// startTime := time.Now()
+			if computedSum, ok := divisorSums[i*j]; ok { // if computedSum exists at index i*j in the map
 				sum += computedSum
 			} else {
+				// compute divisorSum for i * j and add it to the map
 				divisorSum = d(i * j)
 				divisorSums[i*j] = divisorSum
 				sum += divisorSum
 			}
-			fmt.Println("Iteration", j, "after", time.Since(startTime))
+			// fmt.Println("Iteration", j, "after", time.Since(startTime))
 		}
 	}
 	return sum
@@ -40,6 +41,9 @@ var increment int
 
 func d(k int) int {
 	newSum = 0
+	// Avoid checking even divisors for odd values of k.
+	// I think this cuts out 1/4 of the computations d will ever have to do, but will not actually
+	// change the asymptotic behavior of the algorithm.
 	if k%2 == 0 {
 		increment = 1
 	} else {
--- problem439brute.go
+++ problem439brute.go
@@ -11,8 +11,11 @@ func main() {
 	fmt.Println(S(int(math.Pow(10, 11))) % int(math.Pow(10, 9)))
 }
 
+// S(N) returns the sum of particular sums of divisors such that
+// S(N) = ∑1≤i≤N ∑1≤j≤N d(i·j)
 func S(N int) int {
 	sum := 0
+	// This doubly-nested for loop gives T(n) = Θ(n²) right off the bat...
 	for i := 1; i <= N; i++ {
 		for j := 1; j <= N; j++ {
 			sum += d(i * j)
@@ -21,8 +24,12 @@ func S(N int) int {
 	return sum
 }
 
+// d returns the sum of the divisors of k. For example, d(4) returns 1 + 2 + 4 = 7.
 func d(k int) int {
 	sum := 0
+	// Determine the divisors of k via iteration over all numbers up to k. Quite possibly the
+	// slowest way to perform this task. Maybe one of these algorithms would help:
+	// http://en.wikipedia.org/wiki/Integer_factorization#Factoring_algorithms
 	for num := 1; num <= k; num++ {
 		if k%num == 0 {
 			sum += num
